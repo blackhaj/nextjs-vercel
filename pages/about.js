62764b3bb12950885e19fa5abfec69d82cf713f3
Henry Black <blackhaj@gmail.com>
2021-01-25 22:47:09 -0500

    added about

---
+++ pages/about.js
@@ -0,0 +1,2 @@
+const About = () => <h1>Here is the about page</h1>;
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/react-in-jsx-scope */
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
